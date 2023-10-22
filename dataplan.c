fd8c7a7a7bd05eb4dd388416d0517da2df55881c
aryan <aryankhan8654272@gmail.com>
2023-10-22 05:08:28 -0700

    5th commit

--- dataplan.c
+++ dataplan.c
@@ -44,3 +44,21 @@ int main()
 
     printf("%d days used, %d days remaining\n", days_used, days_remaining);
     printf("Average daily use: %.3f GB/day\n", average_daily_use);
+
+    if (average_daily_use <= total_GB / 30.0)
+   
+    {
+        printf("You are UNDER your average daily use (%.3f GB/day).\n", total_GB / 30.0);
+        printf("You can use up to %.3f GB/day to stay within your data plan.\n", total_GB / 30.0);
+    }
+    else
+    {
+        printf("You are EXCEEDING your average daily use (%.3f GB/day).\n", total_GB / 30.0);
+        float excess_usage = average_daily_use - (total_GB / 30.0);
+        float total_excess = excess_usage * days_remaining;
+        printf("Continuing this high usage, you'll exceed your data plan by %.3f GB.\n", total_excess);
+        printf("To stay below your data plan, use no more than %.3f GB/day.\n", total_GB / 30.0);
+    }
+
+    return 0;
+}
