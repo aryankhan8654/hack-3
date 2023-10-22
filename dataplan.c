2f33c99b3cc9c6fc6d9423f31c8cfb6a401bbcbd
aryan <aryankhan8654272@gmail.com>
2023-10-22 04:33:11 -0700

    2nd commit

--- dataplan.c
+++ dataplan.c
@@ -6,3 +6,14 @@ int main()
 
        printf("Enter the total GB in the plan per 30 day period: ");
     scanf("%f", &total_GB);
+
+   
+   
+    if (total_GB <= 0)
+    {
+        printf("Total GB must be greater than 0. Please enter a valid value.\n");
+        return 1;
+    }
+
+    printf("Enter the current day in the 30 day period (1-30): ");
+    scanf("%f", &curr_day);
